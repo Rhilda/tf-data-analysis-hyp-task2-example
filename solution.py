f3c2791c7053c6014f745991174351c28a90668f
Rhilda <ildariismagilov@mail.ru>
2023-04-09 22:09:59 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import anderson_ksamp
-
+from scipy.stats import cramervonmises_2samp
 
 chat_id = 773978697 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    p_value = anderson_ksamp([x, y]).pvalue 
+    result = cramervonmises_2samp(x, y)
+    p_value = result.pvalue
     alpha = 0.08
     return p_value < alpha
