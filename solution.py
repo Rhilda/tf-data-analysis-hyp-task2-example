387cca809d35baf61e8b7d526665fb46eee92265
Rhilda <ildariismagilov@mail.ru>
2023-04-09 13:17:24 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import anderson_ksamp
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 773978697 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    p_value = anderson_ksamp([x, y]).pvalue 
+    alpha = 0.08
+    return p_value < alpha
